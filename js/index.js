398d3ee57f8cb5664fdcf7b3e5872a95887abe67
Rom K <roman28krasko@gmail.com>
2018-03-12 12:44:13 +0000

    next:form

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 $('#submit').click( e => {
-    e.preventDefault();
-    $('#userName').val('');
-    $('#email').val('');
-    $('#textarea').val('');
     $('#tnx').show().animate({ 
         opacity: 1
     },2000);
     $('#tnx').show().animate({ 
         opacity: 0
     },2000);
-})
+});
+
+$(function() {
+    $('.smooth').on('click', function(event) {
+        const target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
